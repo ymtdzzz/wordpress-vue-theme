b47563a2265be51f05df846f15fe3b1197a808d6
zeroclock <zeroclock@icloud.com>
2019-11-30 20:12:12 +0900

    ヘッダー途中

---
+++ src/components/partials/GlobalNav.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="global-nav-container">
+        <div class="global-nav">
+            <ul class="nav">
+                <li>aaaaaaaaaaaaaaaaaaaaaa</li>
+                <li>bbbbbbbbbbbbbbbbbbbbbb</li>
+                <li>cccccccccccccccccccccc</li>
+            </ul>
+        </div>
+        <div class="search-container">
+            <div class="search-form">
+                search form
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .global-nav-container {
+        background-color: gray;
+        padding: 0 10%;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        .global-nav {
+            flex: 1;
+
+            .nav {
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+                text-align: center;
+                margin: 0;
+
+                li {
+                    flex: 1;
+                    line-height: 40px;
+                    list-style: none;
+                    background-color: #0d99d5;
+                    margin: 0;
+                    padding: 0 5px;
+                }
+            }
+        }
+
+        .search-container {
+            flex: 1;
+            text-align: right;
+
+            .search-form {
+                display: inline-block;
+            }
+        }
+    }
+</style>
--- src/components/partials/Header.vue
+++ src/components/partials/Header.vue
@@ -1,3 +1,51 @@
 <template>
-  <header class="site-header">This is the Header</header>
+    <header>
+        <div class="header-container">
+            <div class="header-left-container">
+                ソーシャルボタンとか
+            </div>
+            <p class="site-title">Blog Title</p>
+            <div class="header-right-container">
+                アバウトとか
+            </div>
+        </div>
+        <global-nav></global-nav>
+    </header>
 </template>
+
+<script>
+    import GlobalNav from './GlobalNav'
+
+    export default {
+        components: {
+            GlobalNav
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .header-container {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        padding: 0 20px;
+        height: 70px;
+        background-color: black;
+        color: white;
+        text-align: center;
+
+        .site-title {
+            font-size: 1.5rem;
+            flex: 1;
+        }
+
+        .header-left-container {
+            flex: 1;
+        }
+
+        .header-right-container {
+            flex: 1;
+        }
+    }
+</style>
