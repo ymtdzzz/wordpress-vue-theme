1d151b65567ba580903e54751e2175ac4182789a
zeroclock <zeroclock@icloud.com>
2019-11-30 21:30:07 +0900

    グローバルメニュー一段落

--- src/components/partials/GlobalNav.vue
+++ src/components/partials/GlobalNav.vue
@@ -2,55 +2,113 @@
     <div class="global-nav-container">
         <div class="global-nav">
             <ul class="nav">
-                <li>aaaaaaaaaaaaaaaaaaaaaa</li>
-                <li>bbbbbbbbbbbbbbbbbbbbbb</li>
-                <li>cccccccccccccccccccccc</li>
+                <li><a href="#">aaaaaaaaaaaaaaaaa</a></li>
+                <li><a href="#">bbbbbbbbbbbbbbbbbbbbbb</a></li>
+                <li><a href="#">cccccccccccccccccccccc</a></li>
             </ul>
-        </div>
-        <div class="search-container">
-            <div class="search-form">
-                search form
+            <div class="search-container">
+                <div class="search-form">
+                    search form
+                </div>
             </div>
         </div>
+
     </div>
 </template>
 
 <style lang="scss" scoped>
+    /*.global-nav-container {*/
+
+    /*    background-color: gray;*/
+    /*    display: flex;*/
+    /*    justify-content: space-between;*/
+    /*    align-items: center;*/
+
+    /*    .global-nav {*/
+    /*        width: 80%;*/
+    /*        margin: 0 auto;*/
+    /*        .nav {*/
+    /*            width: 600px;*/
+    /*            display: flex;*/
+    /*            flex-flow: row wrap;*/
+    /*            justify-content: space-between;*/
+    /*            margin: 0;*/
+    /*            height: 40px;*/
+
+    /*            li {*/
+    /*                list-style: none;*/
+    /*                background-color: #0d99d5;*/
+    /*                height: 100%;*/
+    /*                flex-basis: 200px;*/
+    /*                margin-right: 10px;*/
+    /*                display: table;*/
+    /*                a {*/
+    /*                    height: 100%;*/
+    /*                    display: table-cell;*/
+    /*                    vertical-align: middle;*/
+    /*                    text-align: center;*/
+    /*                    padding: 0 10px;*/
+    /*                }*/
+    /*            }*/
+    /*        }*/
+    /*    }*/
+
+    /*    .search-container {*/
+    /*        flex: 1;*/
+    /*        text-align: right;*/
+
+    /*        .search-form {*/
+    /*            display: inline-block;*/
+    /*        }*/
+    /*    }*/
+    /*}*/
+
     .global-nav-container {
-        background-color: gray;
-        padding: 0 10%;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-
+        background-color :pink;
+        height: 40px;
         .global-nav {
-            flex: 1;
-
-            .nav {
-                display: flex;
-                justify-content: space-between;
-                align-items: center;
-                text-align: center;
-                margin: 0;
-
-                li {
-                    flex: 1;
-                    line-height: 40px;
-                    list-style: none;
-                    background-color: #0d99d5;
-                    margin: 0;
-                    padding: 0 5px;
-                }
-            }
+            width: 1000px;
+            height: 100%;
+            margin: 0 auto;
+            display: flex;
+            justify-content: space-between;
         }
+    }
 
-        .search-container {
-            flex: 1;
-            text-align: right;
-
-            .search-form {
-                display: inline-block;
+    .nav {
+        display : flex;
+        flex-flow: row wrap;
+        width: 600px;
+        height: 100%;
+        margin: 0;
+        padding: 0;
+        li {
+            height: 100%;
+            list-style: none;
+            display: table;
+            text-align: center;
+            padding: 0 5px;
+            border-left: 1px solid #fff;
+            &:last-child{
+                border-right: 1px solid #fff;
+            }
+            background-color : gray;
+            a  {
+                display: table-cell;
+                height: 100%;
+                vertical-align: middle;
             }
         }
     }
+    .search-container {
+        display: table;
+        height: 100%;
+        text-align: right;
+
+        .search-form {
+            display: table-cell;
+            vertical-align: middle;
+        }
+    }
+
 </style>
