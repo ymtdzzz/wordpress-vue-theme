19acd2165c77171a80e9b35ba04bdf6d14be4e68
zeroclock <zeroclock@icloud.com>
2019-11-30 21:34:00 +0900

    ゴミ掃除

--- src/components/partials/GlobalNav.vue
+++ src/components/partials/GlobalNav.vue
@@ -17,52 +17,6 @@
 </template>
 
 <style lang="scss" scoped>
-    /*.global-nav-container {*/
-
-    /*    background-color: gray;*/
-    /*    display: flex;*/
-    /*    justify-content: space-between;*/
-    /*    align-items: center;*/
-
-    /*    .global-nav {*/
-    /*        width: 80%;*/
-    /*        margin: 0 auto;*/
-    /*        .nav {*/
-    /*            width: 600px;*/
-    /*            display: flex;*/
-    /*            flex-flow: row wrap;*/
-    /*            justify-content: space-between;*/
-    /*            margin: 0;*/
-    /*            height: 40px;*/
-
-    /*            li {*/
-    /*                list-style: none;*/
-    /*                background-color: #0d99d5;*/
-    /*                height: 100%;*/
-    /*                flex-basis: 200px;*/
-    /*                margin-right: 10px;*/
-    /*                display: table;*/
-    /*                a {*/
-    /*                    height: 100%;*/
-    /*                    display: table-cell;*/
-    /*                    vertical-align: middle;*/
-    /*                    text-align: center;*/
-    /*                    padding: 0 10px;*/
-    /*                }*/
-    /*            }*/
-    /*        }*/
-    /*    }*/
-
-    /*    .search-container {*/
-    /*        flex: 1;*/
-    /*        text-align: right;*/
-
-    /*        .search-form {*/
-    /*            display: inline-block;*/
-    /*        }*/
-    /*    }*/
-    /*}*/
-
     .global-nav-container {
         background-color :pink;
         height: 40px;
