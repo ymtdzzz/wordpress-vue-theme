9dea49f0c1f8bd9f7261da1d1a6ba2e3e948ea25
zeroclock <zeroclock@icloud.com>
2019-12-01 14:58:27 +0900

    フッターの作成

--- src/components/partials/Footer.vue
+++ src/components/partials/Footer.vue
@@ -1,3 +1,42 @@
 <template>
-  <footer class="site-footer">This is the footer.</footer>
+  <div>
+    <div class="site-footer">
+      <div class="footer-widget">
+        Footer Container 1
+      </div>
+      <div class="footer-widget">
+        Footer Container 2
+      </div>
+      <div class="footer-widget">
+        Footer Container 3
+      </div>
+    </div>
+    <footer class="copy-right">
+      © 2019 yMATSUDA All rights reserved.
+    </footer>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+  .site-footer {
+    min-height: 350px;
+    background-color: black;
+    color: white;
+    display: flex;
+
+    .footer-widget {
+      flex: 1;
+      border-right: 1px solid white;
+      padding: 5px;
+      margin: 10px;
+
+      &:last-child {
+        border: none;
+      }
+    }
+  }
+  .copy-right {
+    padding: 5px 0;
+    text-align: center;
+  }
+</style>
--- src/components/partials/GlobalNav.vue
+++ src/components/partials/GlobalNav.vue
@@ -2,9 +2,11 @@
     <div class="global-nav-container">
         <div class="global-nav">
             <ul class="nav">
-                <li><a href="#">aaaaaaaaaaaaaaaaa</a></li>
-                <li><a href="#">bbbbbbbbbbbbbbbbbbbbbb</a></li>
-                <li><a href="#">cccccccccccccccccccccc</a></li>
+                <li><a href="#">AWS</a></li>
+                <li><a href="#">PHP</a></li>
+                <li><a href="#">Mac</a></li>
+                <li><a href="#">Win</a></li>
+                <li><a href="#">Other</a></li>
             </ul>
             <div class="search-container">
                 <div class="search-form">
@@ -18,40 +20,53 @@
 
 <style lang="scss" scoped>
     .global-nav-container {
-        background-color :pink;
-        height: 40px;
+        background-color: #222;
+        height: 60px;
         .global-nav {
-            width: 1000px;
             height: 100%;
             margin: 0 auto;
             display: flex;
             justify-content: space-between;
+            width: 80%;
+
+            .nav {
+                flex: 2;
+            }
+
+            .search-container {
+                flex: 1;
+            }
         }
     }
 
     .nav {
         display : flex;
         flex-flow: row wrap;
-        width: 800px;
         height: 100%;
         margin: 0;
         padding: 0;
         li {
             height: 100%;
-            width: 250px;
+            width: 19%;
             list-style: none;
             display: table;
             text-align: center;
-            border-left: 1px solid #fff;
+            border-right: 1px solid #777;
             &:last-child{
-                border-right: 1px solid #fff;
+                border: none;
             }
-            background-color : gray;
-            a  {
+            a {
                 display: table-cell;
                 height: 100%;
                 vertical-align: middle;
                 padding: 0 5px;
+                transition: background-color 0.2s;
+                text-decoration: none;
+                color: white;
+
+                &:hover {
+                    background-color: #333;
+                }
             }
         }
     }
