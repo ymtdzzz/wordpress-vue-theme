7bbe40d07adaead7037745b1176e3b71896df33a
yosuke MATSUDA <yosuke.matsuda@balconia.co.jp>
2019-12-06 13:26:24 +0900

    コメントの表示

--- src/Constants.js
+++ src/Constants.js
@@ -3,8 +3,8 @@ export default {
 	// This is likely determined by how many dispatched actions you have below
 	// in the created() method
 	LOADING_SEGMENTS: 2,
-	// DEV_DOMAIN: "http://vccw.test",
-	DEV_DOMAIN: "http://localhost",
+	DEV_DOMAIN: "http://vccw.test",
+	// DEV_DOMAIN: "http://localhost",
 	API_BASE_PATH: "/wp-json/wp/v2/",
 	API_MENU_PATH: "/wp-json/menus/v1/",
 	LOCATION_SLUG_HEADER_1: "header_menu_1",		// ヘッダーの固定ページメニューに紐づいたlocationのslug
--- src/components/partials/Comment.vue
+++ src/components/partials/Comment.vue
@@ -1,12 +1,13 @@
 <template>
     <div v-if="isOpen" class="comments-container">
         <h1 class="comments-header">Comments</h1>
-        {{comments}}
         <div class="comment-container" v-for="comment in comments">
             <div class="comment-header">
-                <a class="author-info" v-if="!_.isEmpty(comment.author_url)" :href="comment.author_url">{{ comment.author_name }}</a>
-                <span class="author-info" v-else>{{ comment.author_name }}</span>
+                <a class="author-name" v-if="!(comment.author_url === '')" :href="comment.author_url">{{ comment.author_name }}</a>
+                <span class="author-name" v-else>{{ comment.author_name }}</span>
+                <span class="comment-date"> | {{ comment.date | moment }}</span>
             </div>
+            <div class="comment-content" v-html="comment.content.rendered"></div>
         </div>
     </div>
 </template>
@@ -30,6 +31,29 @@
 
         .comments-header {
             font-size: 2.2rem;
+        }
+
+        .comment-container {
+          margin-top: 20px;
+
+          .comment-header {
+            padding: 5px 10px;
+
+            .author-name {
+              font-size: 1.4rem;
+            }
+
+            .comment-date {
+              color: #555;
+            }
+          }
+        }
+
+        .comment-content {
+          margin-top: 12px;
+          border: 1px solid black;
+          border-radius: 7px;
+          padding: 15px;
         }
     }
 </style>
