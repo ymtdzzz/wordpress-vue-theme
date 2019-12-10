1e4bf8b4dddac23cbf5e926b8439a49a1b151f07
zeroclock <zeroclock@icloud.com>
2019-12-11 08:15:35 +0900

    レスポンシブ

--- src/components/partials/Footer.vue
+++ src/components/partials/Footer.vue
@@ -29,27 +29,4 @@
 </script>
 
 <style lang="scss" scoped>
-  .site-footer {
-    background-color: black;
-    color: white;
-    display: flex;
-    padding: 0 5%;
-
-    .footer-widget {
-      flex: 1;
-      /*border-right: 1px solid white;*/
-      padding: 15px;
-      margin: 10px;
-
-      &:last-child {
-        /*border: none;*/
-      }
-    }
-  }
-  .copy-right {
-    padding: 10px 0;
-    background-color: black;
-    text-align: center;
-    color: white;
-  }
 </style>
