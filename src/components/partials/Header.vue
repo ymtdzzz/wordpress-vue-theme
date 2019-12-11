2608b18f04a0d041ae0d91399c1dd5a224e4c8b2
zeroclock <zeroclock@icloud.com>
2019-12-11 22:40:32 +0900

    グローバルメニュー途中

--- src/components/partials/Header.vue
+++ src/components/partials/Header.vue
@@ -1,9 +1,27 @@
 <template>
     <header>
         <div class="header-container">
-<!--            <div class="header-left-container">-->
-<!--                ソーシャルボタンとか-->
-<!--            </div>-->
+            <div class="nav-drawer">
+                <input id="nav-input" type="checkbox" class="nav-unshown" ref="navinput">
+                <label id="nav-open" for="nav-input"><span></span></label>
+                <label class="nav-unshown" id="nav-close" for="nav-input"></label>
+                <div id="nav-content">
+                    <p class="nav-header">MENU</p>
+                    <div class="search">
+                        <div class="search-container">
+                            <div class="search-form">
+                                <input class="search-input" type="text" placeholder="Search" v-model="search_keyword">
+                                <button class="search-button" @click="execSearch"><v-fa icon="search"/></button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="nav-title">CATEGORY</div>
+                    <div class="nav-content">
+                        カテゴリ
+                    </div>
+
+                </div>
+            </div>
             <router-link :to="{ name: 'Posts' }" class="site-title" v-if="siteLoaded">{{ siteInfo.name }}</router-link>
             <div class="header-right-container">
                 <ul class="page-nav">
@@ -21,6 +39,7 @@
     import GlobalNav from './GlobalNav'
     import Constants from "../../Constants"
     import { mapGetters } from 'vuex'
+    import router from "../../router"
 
     export default {
         components: {
@@ -32,7 +51,25 @@
                 pageMenuLoaded: "pageMenuLoaded",
                 siteInfo: "siteInfo",
                 siteLoaded: "siteLoaded",
+                // ハンバーガーメニュー対応
+                menus: "menus",
+                menuLoaded: "menuLoaded",
+                user: 'user',
+                UserLoaded: 'UserLoaded',
+                footerMenus: "footerMenus",
+                footerMenuLoaded: "footerMenuLoaded",
             })
+        },
+        data() {
+            return {
+                search_keyword: '',
+            }
+        },
+        methods: {
+            execSearch: function () {
+                this.$refs.navinput.checked = false
+                router.push({ name: 'Search', params: { search_keyword: this.search_keyword } })
+            }
         },
         mounted() {
             this.$store.dispatch('getMenuBySlug', { slug: Constants.LOCATION_SLUG_HEADER_1, type: 1 })
