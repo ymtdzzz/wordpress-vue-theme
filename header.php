0b502dcd4da1f9d3a263585ba937b08d79cf6167
zeroclock <zeroclock@icloud.com>
2019-12-14 17:17:28 +0900

    フォント設定

--- header.php
+++ header.php
@@ -7,6 +7,7 @@
       charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700|Noto+Sans+JP:400,700" rel="stylesheet">
     <?php wp_head(); ?>
   </head>
 
