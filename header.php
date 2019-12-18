326ac7fe50c9d9135ce1a54cee18940dee317098
yosuke MATSUDA <yosuke.matsuda@balconia.co.jp>
2019-12-18 12:40:43 +0900

    GAタグ追加

--- header.php
+++ header.php
@@ -3,7 +3,17 @@
 <html <?php language_attributes(); ?> class="no-js no-svg">
 
   <head>
-    <meta
+	  <!-- Global site tag (gtag.js) - Google Analytics -->
+	  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-138253285-1"></script>
+	  <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-138253285-1');
+	  </script>
+
+	  <meta
       charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="http://gmpg.org/xfn/11">
